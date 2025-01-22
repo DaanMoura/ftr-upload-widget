8b8a1ccd9a10a93a48452de3c0b96fb94ed2978a
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-01-22 09:04:23 -0300

    config docker, env and swagger

---
+++ src/infra/http/server.ts
@@ -0,0 +1,59 @@
+import { env } from '@/env'
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+
+const HOST = '0.0.0.0'
+
+const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+// for not handled errors, unexpected errors
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  // recommended to send error for some observability tool (Sentry/Datadog/Grafrana)
+  console.error(error)
+
+  // important to not send sensitive information, so we return a generic message
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0', // Project version, not openapi version
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
+
+server.listen({ port: env.PORT, host: HOST }).then(() => {
+  console.log(`HTTP server running on port ${env.PORT}`)
+})
