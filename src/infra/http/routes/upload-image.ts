59466844e7f4ea8b302c62aa23bc8c4d677ef2c3
Daniel Moura <daniel.lucio.moura@gmail.com>
2025-01-23 09:25:34 -0300

    init upload image function and adjust biome rules

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,4 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
+import { uploadImage } from '@/app/functions/upload-image'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -12,9 +11,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
-          409: z
-            .object({ message: z.string() })
-            .describe('Upload already exists'),
+          400: z.object({ message: z.string() }),
         },
       },
     },
@@ -23,15 +20,17 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         limits: { fileSize: 1024 * 1024 * 2 }, // 2mb
       })
 
-      console.log({ uploadedFile })
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required' })
+      }
 
-      // await db.insert(schema.uploads).values({
-      //   name: 'teste.jpg',
-      //   remoteKey: 'teste.jpg',
-      //   remoteUrl: 'https://example.com/teste.jpg',
-      // })
+      await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
 
-      return reply.status(201).send({ uploadId: '123' })
+      return reply.status(201).send({ uploadId: 'TODO: return upload id' })
     }
   )
 }
